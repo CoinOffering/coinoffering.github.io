a751aaf1f3ca83d433e4fc5341af235b8c1023b7
Viktor <ageyev@gmail.com>
2017-05-16 06:04:15 +0300

    +

--- dapp/contracts/Erc20TokensContract.sol.js
+++ dapp/contracts/Erc20TokensContract.sol.js
@@ -386,7 +386,7 @@ var SolidityEvent = require("web3/lib/web3/event.js");
       }
     ],
     "events": {},
-    "updated_at": 1494902990196
+    "updated_at": 1494903839739
   }
 };
 
--- dapp/contracts/tokenRecipient.sol.js
+++ dapp/contracts/tokenRecipient.sol.js
@@ -376,7 +376,7 @@ var SolidityEvent = require("web3/lib/web3/event.js");
       }
     ],
     "events": {},
-    "updated_at": 1494902990200
+    "updated_at": 1494903839741
   }
 };
 
